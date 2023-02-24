287510ad27cbb3c9cbe1e0e8d6993d58c08953b3
codeWithAkif <121157834+TeknoOtomasyon@users.noreply.github.com>
2023-02-24 17:45:02 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, PostCreateComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, FormsModule, AppRoutingModule,MatInputModule,MatCardModule,MatFormFieldModule,MatButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
